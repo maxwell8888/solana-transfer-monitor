efbcbb9b7f1bb61c0a19a8efc7a48aa53fa90408
Max Unsted <maxunsted@gmail.com>
2024-03-01 01:10:56 +0000

    add tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use serde_json::Value;
 use solana_client::{rpc_client::RpcClient, rpc_config::RpcBlockConfig};
 use solana_sdk::commitment_config::CommitmentConfig;
@@ -15,6 +15,8 @@ use std::{
     time::{Duration, Instant},
 };
 pub mod utils;
+use tracing::{debug, info, trace};
+use tracing_subscriber::EnvFilter;
 
 const USDC_MINT_ADDRESS: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 
@@ -31,10 +33,8 @@ fn write_transaction_transfers<W: Write>(
     writer: &mut W,
 ) -> Result<()> {
     let signature = match &transaction.transaction {
-        EncodedTransaction::LegacyBinary(_) => todo!(),
-        EncodedTransaction::Binary(_, _) => todo!(),
         EncodedTransaction::Json(ui_transaction) => ui_transaction.signatures.clone(),
-        EncodedTransaction::Accounts(_) => todo!(),
+        _ => bail!("expected EncodedTransaction::Json"),
     };
 
     let mut accounts_map = HashMap::new();
@@ -42,9 +42,9 @@ fn write_transaction_transfers<W: Write>(
     let parsed_accounts = match transaction.transaction {
         EncodedTransaction::Json(ui_transaction) => match ui_transaction.message {
             UiMessage::Parsed(ui_parsed_message) => ui_parsed_message.account_keys,
-            UiMessage::Raw(_) => todo!(),
+            _ => bail!("expected UiMessage::Parsed"),
         },
-        _ => todo!(),
+        _ => bail!("expected EncodedTransaction::Json"),
     };
 
     if let Some(meta) = transaction.meta {
@@ -59,26 +59,25 @@ fn write_transaction_transfers<W: Write>(
                                 .clone();
                             let owner = match pre_ui_transaction_token_balance.owner {
                                 OptionSerializer::Some(owner) => owner,
-                                _ => todo!(),
+                                _ => bail!("expected OptionSerializer::Some"),
                             };
                             accounts_map
                                 .insert(pub_key, (owner, pre_ui_transaction_token_balance.mint));
                         }
                     }
                 }
-                OptionSerializer::None => todo!(),
-                OptionSerializer::Skip => todo!(),
+                _ => bail!("expected OptionSerializer::Some"),
             }
 
-            let mut print_sig = true;
-
+            let mut first_transfer = true;
+            
             match meta.inner_instructions {
                 OptionSerializer::Some(intructions_vec) => {
                     for instructions in intructions_vec {
                         for instruction in instructions.instructions {
                             match instruction {
                                 UiInstruction::Compiled(_) => {
-                                    todo!()
+                                    bail!("expected UiInstruction::Parsed")
                                 }
                                 UiInstruction::Parsed(ui_instruction_parsed) => {
                                     match ui_instruction_parsed {
@@ -89,10 +88,9 @@ fn write_transaction_transfers<W: Write>(
                                                     &mut accounts_map,
                                                 )?;
                                                 if let Some(transfer) = transfer {
-                                                    if print_sig {
-                                                        println!("");
-                                                        println!("signature: {signature:?}");
-                                                        print_sig = false;
+                                                    if first_transfer {
+                                                        debug!("tx signature: {signature:?}");
+                                                        first_transfer = false;
                                                     }
                                                     transfer.write(writer)?;
                                                 }
@@ -105,8 +103,7 @@ fn write_transaction_transfers<W: Write>(
                         }
                     }
                 }
-                OptionSerializer::None => todo!(),
-                OptionSerializer::Skip => todo!(),
+                _ => bail!("expected OptionSerializer::Some"),
             }
         }
     }
@@ -125,19 +122,32 @@ pub fn write_block_transfers<W: Write>(
             write_transaction_transfers(transaction, writer)?;
         }
     } else {
-        eprintln!("Error: no transactions found for block in slot {slot}");
+        info!("no transactions found for block in slot {slot}");
     }
 
     Ok(())
 }
 
 pub fn run() -> Result<()> {
+    if let Ok(level) = std::env::var("RUST_LOG") {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::new(&format!("solana_transfer_monitor={level}")))
+            .init();
+    }
+
     let rpc_url = "https://api.mainnet-beta.solana.com".to_string();
     // let rpc_url = "https://api.devnet.solana.com".to_string();
     let client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::finalized());
 
+    // Possible solution to rate limiting but doesn't appear to work for get_block
+    // let client = RpcClient::new_with_timeout_and_commitment(
+    //     rpc_url,
+    //     Duration::from_secs(2),
+    //     CommitmentConfig::finalized(),
+    // );
+
     let rpc_block_config = make_block_config();
-    let mut current_slot = client.get_slot().unwrap();
+    let mut current_slot = client.get_slot()?;
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
@@ -149,22 +159,23 @@ pub fn run() -> Result<()> {
 
         // Reset rate limit period and request count every 10 seconds
         if rate_limit_period_start.elapsed() > Duration::from_secs(10) {
-            println!("reset rate limit");
+            trace!("reset rate limit");
             rate_limit_period_start = Instant::now();
             request_count = 0;
         }
         // if request_count > 99 {
         if request_count > 39 {
             // Exceeded rate limit so Wait a while before the next iteration to avoid rate limiting
-            println!("exceeded rate limit");
+            trace!("exceeded rate limit");
             sleep(Duration::from_millis(1000));
             continue;
         }
         request_count += 1;
 
-        let slots = client.get_blocks(current_slot, None).unwrap();
+        let slots = client.get_blocks(current_slot, None)?;
         // println!("{slots:?}");
-        println!("slots.len(): {}", slots.len());
+        // println!("slots.len(): {}", slots.len());
+        debug!("slots.len(): {}", slots.len());
         if let Some(last_slot) = slots.last() {
             current_slot = last_slot + 1;
         }
@@ -172,75 +183,66 @@ pub fn run() -> Result<()> {
         for slot in slots.clone() {
             // Reset rate limit period and request count every 10 seconds
             if rate_limit_period_start.elapsed() > Duration::from_secs(10) {
-                println!("reset rate limit");
+                trace!("reset rate limit");
                 rate_limit_period_start = Instant::now();
                 request_count = 0;
             }
             if request_count > 99 {
                 // Exceeded rate limit so Wait a while before the next iteration to avoid rate limiting
-                println!("exceeded rate limit");
+                trace!("exceeded rate limit");
                 sleep(Duration::from_millis(1000));
                 continue;
             }
             request_count += 1;
 
             let get_block_start = Instant::now();
-            let block = client
-                .get_block_with_config(slot, rpc_block_config)
-                .unwrap();
-            let get_block_elapsed = get_block_start.elapsed();
-            // dbg!(get_block_elapsed);
+            trace!("request block {slot}");
+            let block = client.get_block_with_config(slot, rpc_block_config)?;
+            trace!(
+                "get_block_with_config took: {:?}",
+                get_block_start.elapsed()
+            );
             write_block_transfers(block, slot, &mut handle)?;
             // sleep(Duration::from_millis(1000));
         }
 
         // current_slot += 1; // Move to the next slot
 
-        let elapsed = iteration_start.elapsed();
+        trace!("loop iteration elapsed in {:?}", iteration_start.elapsed());
     }
 }
 
-// >8 -> xx.xx (remove decimals if 00)
+// >8 -> x,xxx,xxx.xx (remove decimals if 00)
 // 7 -> x.xxx x
 // 6 -> 0.xxx xxx
 // 5 -> 0.0xx xxx
 // 4 -> 0.00x xxx
 // etc
-fn format_amount(raw_amount: &str) -> Option<String> {
-    let amount = if raw_amount.len() > 7 {
-        let pre_decy = raw_amount[..raw_amount.len() - 6]
-            .as_bytes()
-            .rchunks(3)
-            .rev()
-            .map(from_utf8)
-            .collect::<Result<Vec<&str>, _>>()
-            .unwrap()
-            .join(",");
-        let post_decy = &raw_amount[raw_amount.len() - 6..raw_amount.len() - 4];
-        if post_decy == "00" {
-            pre_decy
-        } else {
-            format!("{}.{}", pre_decy, post_decy)
+fn format_amount(raw_amount: &str) -> Result<String> {
+    Ok(match raw_amount.len() {
+        7 => format!("{}.{}", &raw_amount[..1], &raw_amount[1..5]),
+        6 => format!("0.{raw_amount}"),
+        5 => format!("0.0{raw_amount}"),
+        4 => format!("0.00{raw_amount}"),
+        3 => format!("0.000{raw_amount}"),
+        2 => format!("0.000{raw_amount}"),
+        1 => raw_amount.to_string(),
+        _ => {
+            let pre_decy = raw_amount[..raw_amount.len() - 6]
+                .as_bytes()
+                .rchunks(3)
+                .rev()
+                .map(from_utf8)
+                .collect::<Result<Vec<&str>, _>>()?
+                .join(",");
+            let post_decy = &raw_amount[raw_amount.len() - 6..raw_amount.len() - 4];
+            if post_decy == "00" {
+                pre_decy
+            } else {
+                format!("{}.{}", pre_decy, post_decy)
+            }
         }
-    } else if raw_amount.len() == 7 {
-        format!("{}.{}", &raw_amount[..1], &raw_amount[1..5])
-    } else if raw_amount.len() == 6 {
-        format!("0.{raw_amount}")
-    } else if raw_amount.len() == 5 {
-        format!("0.0{raw_amount}")
-    } else if raw_amount.len() == 4 {
-        format!("0.00{raw_amount}")
-    } else if raw_amount.len() == 3 {
-        format!("0.000{raw_amount}")
-    } else if raw_amount.len() == 2 {
-        format!("0.000{raw_amount}")
-    } else if raw_amount == "0" {
-        format!("0")
-    } else {
-        dbg!(raw_amount);
-        return None;
-    };
-    Some(amount)
+    })
 }
 pub struct Transfer {
     pub source_owner: String,
@@ -268,7 +270,6 @@ fn handle_parsed_instruction(
     mut parsed_instruction: Value,
     accounts_map: &mut HashMap<String, (String, String)>,
 ) -> Result<Option<Transfer>> {
-    let parsed_instruction_copy = parsed_instruction.clone();
     let type_ = parsed_instruction["type"].take();
     let type_ = type_
         .as_str()
@@ -303,27 +304,20 @@ fn handle_parsed_instruction(
                 let raw_amount = info["amount"].take();
                 let message = "amount not found in instruction JSON";
                 (raw_amount, message)
-            } else if type_ == "transferChecked" {
+            } else {
                 let mut token_amount = info["tokenAmount"].take();
                 let raw_amount = token_amount["amount"].take();
                 let message = "amount not found in tokenAmount JSON";
                 (raw_amount, message)
-            } else {
-                panic!("unexpected instruction type");
             };
             let raw_amount = raw_amount.as_str().context(message)?;
 
-            let formatted_amount = format_amount(raw_amount);
-            if let Some(formatted_amount) = formatted_amount {
-                return Ok(Some(Transfer {
-                    source_owner: source_owner.clone(),
-                    destination_owner: destination_owner.clone(),
-                    formatted_amount,
-                }));
-            } else {
-                dbg!(parsed_instruction_copy);
-                return Err(anyhow!("fucked"));
-            }
+            let formatted_amount = format_amount(raw_amount)?;
+            return Ok(Some(Transfer {
+                source_owner: source_owner.clone(),
+                destination_owner: destination_owner.clone(),
+                formatted_amount,
+            }));
         }
     }
     Ok(None)
